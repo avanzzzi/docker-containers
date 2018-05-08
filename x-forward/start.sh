4e62a31c8090b9ca6cbed44f2978eccb84e92d1c
Gabriel Avanzi <gabriel.avanzi@gmail.com>
2018-05-08 10:48:09 -0300

    Update x-forward image

--- x-forward/start.sh
+++ x-forward/start.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
+
 # prepare xhost
 xhost +local:
 
-# prepare xauth
+# bind mount xsock
 XSOCK=/tmp/.X11-unix
-#XAUTH=/tmp/.docker.xauth
-#xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 # run docker
-# docker run --rm -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY $1
 docker run --rm -ti -v $XSOCK:$XSOCK -e DISPLAY=$DISPLAY $1
 
-# prepare xhost
+# restore xhost
 xhost -local:
